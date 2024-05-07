02227f10e070d38b40f791b158a203d8a560d208
Jacquelyn <jacquelyns131@gmail.com>
2024-05-07 16:35:47 -0400

    initial commit

---
+++ backend/ukg-time/src/test/java/com/example/ukgtime/UkgTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukgtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkgTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
