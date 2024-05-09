40f9696286557f2be0f208d75b368f881c6e9616
Folasade Goddard <folasade.goddard@gmail.com>
2024-05-09 12:06:29 -0400

    Added routes for different pages in frontend.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App.jsx';
+import Root from './routes/Root.jsx';
+import ErrorPage from './ErrorPage.jsx';
+import SignUp from './components/SignUp.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <App /> },
+      { path: '*', element: <ErrorPage />, error: true, message: 'Not Found'},
+      { path: '/signup', element: <SignUp />}
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
