a5225cdf9fb9beef6da308b5451b488ae1b5ba0b
Jacquelyn <jacquelyns131@gmail.com>
2024-05-10 15:04:05 -0400

    create empty class files

---
+++ backend/ukg-time/src/main/java/com/example/ukgtime/ProfileImage.java
@@ -0,0 +1,16 @@
+package com.example.ukgtime;
+
+public class ProfileImage {
+    private long eId;
+    private byte[] profileImage;
+
+    @Override
+    public String toString() {
+        return String.format();
+    }
+
+    public ProfileImage(long eId, byte[] profileImage) {
+        this.eId = eId;
+        this.profileImage = profileImage;
+    }
+}
