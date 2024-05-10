eb6c3266903ab9775963db58eb32ea8bcbd4b4d5
Jacquelyn <jacquelyns131@gmail.com>
2024-05-10 16:55:21 -0400

    work on classes for tables

--- backend/ukg-time/src/main/java/com/example/ukgtime/ProfileImage.java
+++ backend/ukg-time/src/main/java/com/example/ukgtime/ProfileImage.java
@@ -6,11 +6,28 @@ public class ProfileImage {
 
     @Override
     public String toString() {
-        return String.format();
+        return String.format("ProfileImage[eId: %d, profileImage: '%s']",
+                eId, profileImage==null ? "":profileImage.toString() );
     }
 
     public ProfileImage(long eId, byte[] profileImage) {
         this.eId = eId;
+        this.profileImage = profileImage;
+    }
+
+    public long geteId() {
+        return eId;
+    }
+
+    public void seteId(long eId) {
+        this.eId = eId;
+    }
+
+    public byte[] getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(byte[] profileImage) {
         this.profileImage = profileImage;
     }
 }
