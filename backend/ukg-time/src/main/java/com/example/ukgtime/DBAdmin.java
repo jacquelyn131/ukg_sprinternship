e21fda79cf7fb03838451fc0e92a80d8bad0fc37
Jacquelyn <jacquelyns131@gmail.com>
2024-05-11 14:05:27 -0400

    work on dbadmin inserting into tables

--- backend/ukg-time/src/main/java/com/example/ukgtime/DBAdmin.java
+++ backend/ukg-time/src/main/java/com/example/ukgtime/DBAdmin.java
@@ -14,7 +14,17 @@ import java.util.Arrays;
 
 public class DBAdmin {
     public static final Logger log = LoggerFactory.getLogger(UkgTimeApplication.class);
-    public void setUpTables() {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public void setUpTables() throws Exception {
+
         log.info("setting up tables...");
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS company");
+        jdbcTemplate.execute("CREATE TABLE company(" +
+                "company_id SERIAL PRIMARY KEY NOT NULL, company_name String, headquarters_id INTEGER)");
+        // insert example companies into table: UKG, Santa's Workshop, Kittens Inc.
+        List<Object[]> companies = Arrays.asList("UKG", 1)
     }
 }
