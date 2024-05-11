ccea1e42b4bf6734ccabedf2e770d04f6a901c44
folasadeee <folasade.goddard@gmail.com>
2024-05-10 22:39:22 -0400

    Added styling for navbar

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Outlet, Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Outlet } from "react-router-dom";
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+import styles from './Layout.module.css';
+import classNames from 'classnames';
 
 const Layout = () => {
+
+  const logoPath =  
+  './UKGtimelogo.svg'; 
+
+  const [navbarHeight, setNavbarHeight] = useState(0);
+
+  useEffect(() => {
+    const navbar = document.getElementById('navbar');
+    if (navbar) {
+      setNavbarHeight(navbar.offsetHeight);
+      document.body.style.paddingTop = `${navbar.offsetHeight}px`;
+    }
+  }, []);
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleSignOut = () => {
+    setIsAuthenticated(false);
+  }
+
   return (
    <>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/signin">Sign In</Link>
-    </nav>
+
+
+<Navbar id="navbar" expand="lg" 
+ fixed="top"
+                className={classNames("container-fluid", styles.navbar)}> 
+            <Navbar.Brand href="/"> 
+                <img src={logoPath}
+                     alt="Logo" 
+                     id={styles.navbarLogo}/> 
+              
+            </Navbar.Brand> 
+
+            <Navbar.Toggle  
+                aria-controls="basic-navbar-nav" /> 
+            <Navbar.Collapse  
+                id="basic-navbar-nav"> 
+                <Nav className={classNames("ms-auto", styles.navlinksContainer)}> 
+                <Nav.Link href="/" className={classNames(styles.navLink)}> 
+                        Home
+                    </Nav.Link> 
+                    <Nav.Link href="#features" className={classNames(styles.navLink)}> 
+                        Features
+                    </Nav.Link> 
+                
+                 
+            <Button className={styles.navbarButton}href="/signin">Log In</Button>
+        
+           
+                </Nav> 
+            </Navbar.Collapse> 
+        </Navbar> 
+
+    <main>
     <Outlet />
+      </main>
    </>
   );
 }
