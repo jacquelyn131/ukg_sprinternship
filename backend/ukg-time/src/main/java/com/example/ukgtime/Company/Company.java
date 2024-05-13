45dc20e0264d37ec08c2c633926254cebfc4921a
Rafael Ojeda <rafael.e.ojeda@gmail.com>
2024-05-13 16:36:43 -0400

    user info httprequest

---
+++ backend/ukg-time/src/main/java/com/example/ukgtime/Company/Company.java
@@ -0,0 +1,43 @@
+package com.example.ukgtime.Company;
+
+public class Company {
+    private long companyId;
+    private String companyName;
+    private long headquartersId;
+
+    @Override
+    public String toString() {
+        return String.format("Company[companyId: %d, companyName: '%s', " +
+                "headquartersId: %d]", companyId, companyName, headquartersId);
+    }
+
+    public Company(long companyId, String companyName, long headquartersId) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.headquartersId = headquartersId;
+    }
+
+    public long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(long companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public long getHeadquartersId() {
+        return headquartersId;
+    }
+
+    public void setHeadquartersId(long headquartersId) {
+        this.headquartersId = headquartersId;
+    }
+}
