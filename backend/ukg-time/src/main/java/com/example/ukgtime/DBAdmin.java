8c27a0d41ebcb47b9698a2d1ae893babc13d08b0
Jacquelyn <jacquelyns131@gmail.com>
2024-05-13 09:57:57 -0400

    work on company table

--- backend/ukg-time/src/main/java/com/example/ukgtime/DBAdmin.java
+++ backend/ukg-time/src/main/java/com/example/ukgtime/DBAdmin.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Arrays;
@@ -25,6 +26,24 @@ public class DBAdmin {
         jdbcTemplate.execute("CREATE TABLE company(" +
                 "company_id SERIAL PRIMARY KEY NOT NULL, company_name String, headquarters_id INTEGER)");
         // insert example companies into table: UKG, Santa's Workshop, Kittens Inc.
-        List<Object[]> companies = Arrays.asList("UKG", 1)
+        //  using batch update with a list of objects
+        Company ukg = new Company(1, "UKG", 12);
+        Company santasWorkshop = new Company(2, "Santa's Workshop", 13);
+        Company kittensInc = new Company(3, "Kittens Inc.", 14);
+        List<Company> companies = Arrays.asList(ukg, santasWorkshop, kittensInc);
+        companies.forEach(company->
+                log.info(String.format("Inserting company record for %s", company.getCompanyName())));
+
+        // use jdbctemplate batchupdate to insert multiple
+        // insert statements to insert multiple
+        jdbcTemplate.update(
+                "INSERT INTO company (company_id, company_name, headquarters_id) " +
+                        "VALUES (?, ?, ?), " +
+                        "(?, ?, ?), " +
+                        "(?, ?, ?); ", ukg.getCompanyId(), ukg.getCompanyName(), ukg.getHeadquartersId(),
+                santasWorkshop.getCompanyId(), santasWorkshop.getCompanyName(), santasWorkshop.getHeadquartersId(),
+                kittensInc.getCompanyId(), kittensInc.getCompanyName(), kittensInc.getHeadquartersId());
+        )
+
     }
 }
