d9a268dc7aadce73206e45c91fbfb92d4ec87ed8
Amarise <amarise.rod@gmail.com>
2024-05-14 09:56:04 -0400

    Attendance block

--- frontend/src/pages/EmployeeDashboard/components/attendanceCard/AttendanceCard.jsx
+++ frontend/src/pages/EmployeeDashboard/components/attendanceCard/AttendanceCard.jsx
@@ -9,23 +9,25 @@ const AttendanceCard = (props) => {
         <div className={styles.attendanceBlock}>
             <div className={styles.date}>
                 May 6, 2024
-                </div>
+            </div>
 
            <div className={styles.punchContainer}>
                <div className={styles.totalHours}>
-                   08:00<br />
-                   Total Hours
+                     <div className={styles.num8}>08:00<br /></div>
+                   <h3>Total Hours</h3>
                    </div>
-                    <div className={styles.clockIn}>06:00<br />
-                    Clock In</div>
-                      <div className={styles.clockOut}>02:00<br />
-                      Clock out</div>
+                    <div className={styles.clockIn}>
+                        <div className={styles.num6}>06:00<br/></div>
+                    <h4>Clock In</h4>
+                    </div>
+                      <div className={styles.clockOut}>
+                          <div className={styles.num2}>02:00<br/></div>
+                      <h4>Clock out</h4>
+                    </div>
 
                </div>
-</div>
-</div>
-
-
-        </>)}
+           </div>
+        </div>
+</>)}
 
         export default AttendanceCard;
