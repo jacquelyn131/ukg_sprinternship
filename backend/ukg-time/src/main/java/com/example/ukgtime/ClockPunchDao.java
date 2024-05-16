74ec9fd43d23c1fef9df30386288709558662415
Jacquelyn <jacquelyns131@gmail.com>
2024-05-16 16:14:26 -0400

    fix bug where retrieving a datetime would only get the date, not the time

--- backend/ukg-time/src/main/java/com/example/ukgtime/ClockPunchDao.java
+++ backend/ukg-time/src/main/java/com/example/ukgtime/ClockPunchDao.java
@@ -23,7 +23,7 @@ public class ClockPunchDao implements CorporateEventDao<ClockPunch>{
     private RowMapper<ClockPunch> rowMapper = (rs, rowNum) -> {
         ClockPunch clockPunch = new ClockPunch();
         clockPunch.setPunchId(rs.getLong("punch_id"));
-        clockPunch.setDateTime("" + rs.getDate("date_time"));
+        clockPunch.setDateTime("" + rs.getTimestamp("date_time"));
         clockPunch.setEmployeeId(rs.getLong("employee_id"));
         clockPunch.setOfficeId(rs.getLong("office_id"));
         clockPunch.setType(rs.getString("type"));
@@ -175,7 +175,8 @@ public class ClockPunchDao implements CorporateEventDao<ClockPunch>{
     public List<ClockPunch> employeePunchList(long id) {
         String sql = "SELECT date_time, punch_id, employee_id, office_id, type, valid, comments " +
                 "FROM clock_punch " +
-                "WHERE employee_id = ?";
+                "WHERE employee_id = ? " +
+                "ORDER BY date_time DESC";
         return jdbcTemplate.query(sql, new Object[] {id}, rowMapper);
     }
     // return shift length given two punch id's
