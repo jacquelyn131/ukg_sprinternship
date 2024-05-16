6d069ee8f029f2bc4710810b6475690905d1ae37
Folasade Goddard <folasade.goddard@gmail.com>
2024-05-16 13:33:26 -0400

    Added conditional logic to change dashboard greeting based on time of day

--- frontend/src/pages/EmployeeDashboard/components/greetingMessage/GreetingMessage.jsx
+++ frontend/src/pages/EmployeeDashboard/components/greetingMessage/GreetingMessage.jsx
@@ -1,11 +1,34 @@
 import styles from './GreetingMessage.module.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 
 const GreetingMessage = (props) => {
 
 
-    return <h1 className={styles.greeting}>Good morning, {props.firstName}!</h1>
-    }
+    const [greeting, setGreeting] = useState("Hello");
+
+useEffect(() => {
+
+      const currentTime = new Date().toLocaleTimeString();
+ console.log(currentTime)
+ if (currentTime > "12:00:00 AM" && currentTime < "11:59:59 AM") {
+            setGreeting("Good Morning");
+        } else if (currentTime > "12:00:00 PM" && currentTime < "5:59:59 PM") {
+            setGreeting("Good Afternoon");
+        } else if (currentTime > "6:00:00 PM" && currentTime < "11:59:59 PM") {
+            setGreeting("Good Evening");
+        }  else {
+            setGreeting("Hello");
+        }
+    }, []);
+
+
+return (
+    <div className={styles.greetingMessage}>
+        <h1 className={styles.greeting}>{greeting}, {props.firstName}!</h1>
+    </div>
+);
+}
 
 export default GreetingMessage;
