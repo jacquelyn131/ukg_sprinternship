8eb2a6230c00b3361511de1c9b3b7718d5f99db4
Jacquelyn <jacquelyns131@gmail.com>
2024-05-16 16:03:14 -0400

    work on retrieving a list of punches for an employee

--- backend/ukg-time/src/main/java/com/example/ukgtime/ClockPunchDao.java
+++ backend/ukg-time/src/main/java/com/example/ukgtime/ClockPunchDao.java
@@ -172,6 +172,12 @@ public class ClockPunchDao implements CorporateEventDao<ClockPunch>{
         }
         return Optional.ofNullable(dateTime);
     }
+    public List<ClockPunch> employeePunchList(long id) {
+        String sql = "SELECT date_time, punch_id, employee_id, office_id, type, valid, comments " +
+                "FROM clock_punch " +
+                "WHERE employee_id = ?";
+        return jdbcTemplate.query(sql, new Object[] {id}, rowMapper);
+    }
     // return shift length given two punch id's
     public float getShiftLength(long startId, long endId) throws ParseException {
         float shiftLength = (float)0.0;
